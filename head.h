64d30780cf1908eaa20de4f83c65b82e25eb1ad0
Hayden <haydendonald@gmail.com>
2019-10-18 16:32:26 +1100

    Bug fix

--- head.h
+++ head.h
@@ -165,6 +165,10 @@ class Stepper {
     //Move the stepper in a given direction at speed. Value is a float between -100.0% -> 100.0%. Acceleration is the acceleration again in percentage
     long aimedStopPosition = 0;
     void move(float value, float globalSpeed=50.0, float acceleration=50.0) {
+        //Make sure the globa speed and acceleration cannot go below 0 as this causes issues
+        if(globalSpeed < 0){globalSpeed = 0;}
+        if(acceleration < 0){acceleration = 0;}
+
         //Set the speed
         if(value != 0) {
             _stepper.setAcceleration(_defaultAcceleration * (acceleration/50.0));
